fd1c0985c4ebc037626394000bde9e3934cc3a25
zhenishkaziev <artykbaevaslan0@gmail.com>
2022-01-11 17:17:15 +0300

    интерфейс репоситори для запроса по clean architucture

--- app/src/main/java/com/example/soskarikcyandmorty/domain/repositories/CharacterRepository.kt
+++ app/src/main/java/com/example/soskarikcyandmorty/domain/repositories/CharacterRepository.kt
@@ -4,7 +4,7 @@ import com.example.soskarikcyandmorty.common.Resource
 import com.example.soskarikcyandmorty.domain.models.CharacterModel
 import kotlinx.coroutines.flow.Flow
 
-interface CharacterRepository  {
+interface CharacterRepository {
 
-    fun fetchCharacter(page : Int) : Flow<Resource<List<CharacterModel>>>
+    fun fetchCharacterID(id: Int): Flow<Resource<CharacterModel>>
 }
