9d8a049f70b50bf55322cb172b38a5d014d3f61b
zhenishkaziev <artykbaevaslan0@gmail.com>
2022-01-11 17:11:16 +0300

    общий класс для запросов

--- app/src/main/java/com/example/soskarikcyandmorty/bases/BaseRepository.kt
+++ app/src/main/java/com/example/soskarikcyandmorty/bases/BaseRepository.kt
@@ -1,7 +1,10 @@
 package com.example.soskarikcyandmorty.bases
 
-import androidx.lifecycle.liveData
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.soskarikcyandmorty.common.Resource
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 abstract class BaseRepository {
@@ -17,5 +20,27 @@ abstract class BaseRepository {
                 )
             )
         }
+    }
+
+    protected fun <ValueDto : Any, Value : Any> doPagingRequest(
+        pagingSource: BasePagingSource<ValueDto, Value>,
+        pageSize: Int = 10,
+        prefetchDistance: Int = pageSize,
+        enabledPlaceholder: Boolean = true,
+        initialLoadSize: Int = pageSize * 3,
+        maxSize: Int = Int.MAX_VALUE,
+        jumpThreshold: Int = Int.MIN_VALUE,
+    ): Flow<PagingData<Value>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize,
+                prefetchDistance,
+                enabledPlaceholder,
+                initialLoadSize,
+                maxSize,
+                jumpThreshold
+            ),
+            pagingSourceFactory = { pagingSource }
+        ).flow
     }
 }
